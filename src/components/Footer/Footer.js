7b446b6a0f90bdf44715a802ae48edd3103ddeaf
fang-tanbamrung <tanbamrung_w@hotmail.com>
2018-12-05 21:34:31 +0700

    change footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,9 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className='bt b--white-20'>
-            <div>
-                <a className='no-underline white dim' href='https://github.com/fang-tanbamrung'>GitHub</a>
-            </div>
-            <div>
-
+            <div className='tc mt3'>
+                <a className='ba br2 pa2 no-underline white dim ma2 hover-bg-white-70 hover-black' href='https://icons8.com/'> Icons </a>
+                <a className='ba br2 pa2 no-underline white dim ma2 hover-bg-white-70 hover-black' href='https://github.com/fang-tanbamrung/TodoWebApp'>GitHub</a>
             </div>
         </div>
     )
